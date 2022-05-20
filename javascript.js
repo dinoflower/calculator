53f72e2a46670355d8b51e97d852922e36a53f46
dinoflower <kaedilove@gmail.com>
2022-05-20 16:26:55 -0500

    begin implementing display

--- javascript.js
+++ javascript.js
@@ -1,33 +1,26 @@
 let input;
 let key;
 
+function display(value) {
+    let output = document.querySelector(".text-content"); //apparently the text will count as a child of the box on its own so you don't need this step
+    if(isNaN(output.textContent)) {
+        output.replaceWith(value);
+    }
+    else {
+        newDigit = Document.createTextNode(value);
+        output.appendChild(newDigit);
+    }
+};
+
 function getInput(event) {
-    key = document.querySelector(`button[data-key="${event.keyCode}"]`);
-    input = event.target.textContent;
-    //on first input: if it's clear, enter, or back, nothing happens
-        //if it's a number, it displays the number, replacing the dummy value
-        //if it's a decimal, append the decimal after the existing zero
-        //if it's an operand, treat 0 as the starting value for the operation
-    //on second input, if first was a number:
-        //clear and back both clear input, enter causes no change
-        //if it's a number, append it after the number
-        //if it's a decimal, append the decimal after the number and turn off button
-        //if it's an operand, store it along with the number
-    //on second input, if first was a decimal:
-        //clear and back both clear input, enter causes no change
-        //if it's a number, append it after the decimal
-        //if it's an operand, cause no change (don't store!)
-    //on second input, if first was an operand:
-        //clear and back both clear input, enter causes no change
-        //if it's a number, display the number, replacing the dummy value
-        //if it's a decimal, append the decimal after a 0
-    //continue to append numbers to the end of existing numbers
-    //num1+num2 will display num2 until the user inputs a new operand or =
-    return key;
+    let value = event.target.textContent;
+    display(value);
+    //array of objects? each object is a number/digit input so 1: a, 2: b - you can always just call the value of an object...
 };
 
 //let displayedNum = document.createTextNode();
-document.querySelector(".output-box");
+//document.querySelector(".output-box");
+//key = document.querySelector(`button[data-key="${event.keyCode}"]`);
 
 
 function add (a, b) {
@@ -65,6 +58,29 @@ function operate (operator, number, number) {
     };
 };
 
-const btns = Array.from(document.querySelectorAll(".button"));
-btns.forEach(btn => btn.addEventListener("click", getInput));
-window.addEventListener("keydown", getInput);
+const numBtns = Array.from(document.querySelectorAll(".number"));
+numBtns.forEach(numBtn => numBtn.addEventListener("click", getInput));
+//window.addEventListener("keydown", getInput);
+
+
+
+
+//on first input: if it's clear, enter, or back, nothing happens
+        //if it's a number, it displays the number, replacing the dummy value
+        //if it's a decimal, append the decimal after the existing zero
+        //if it's an operand, treat 0 as the starting value for the operation
+    //on second input, if first was a number:
+        //clear and back both clear input, enter causes no change
+        //if it's a number, append it after the number
+        //if it's a decimal, append the decimal after the number and turn off button
+        //if it's an operand, store it along with the number
+    //on second input, if first was a decimal:
+        //clear and back both clear input, enter causes no change
+        //if it's a number, append it after the decimal
+        //if it's an operand, cause no change (don't store!)
+    //on second input, if first was an operand:
+        //clear and back both clear input, enter causes no change
+        //if it's a number, display the number, replacing the dummy value
+        //if it's a decimal, append the decimal after a 0
+    //continue to append numbers to the end of existing numbers
+    //num1+num2 will display num2 until the user inputs a new operand or =
