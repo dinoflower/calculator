1b13d9e52e127e3d5f10d2a111468cf95f8ed1b8
dinoflower <kaedilove@gmail.com>
2022-05-23 18:26:42 -0500

    fix calculations upon calling with equal button, fix display

--- javascript.js
+++ javascript.js
@@ -1,16 +1,54 @@
 let runningArray = [];
-let output;
 let operator;
+let output;
 
-function createText(content) {
-    text = document.createElement("p");
-    text.setAttribute("class", "text-content");
-    text.textContent = content;
+function doMath () {
+    if (runningArray.length >= 1 && operator !== undefined) {
+        output = document.querySelector(".text-content");
+        arrayItem = Number(output.textContent);
+        runningArray.push(arrayItem);
+        let result = operate(operator, runningArray[runningArray.length-2], runningArray[runningArray.length-1]);
+        createText(result);
+        text.setAttribute("name", "noAppend");
+        output.replaceWith(text);
+    }
+    operator = undefined;
 };
 
-function display(value) {
+function addPoint() {
     output = document.querySelector(".text-content");
-    if(isNaN(output.textContent)) {
+    if (isNaN(output.textContent)) {
+        createText("0.");
+        output.replaceWith(text);
+    }
+    else if (Number.isInteger(Number(output.textContent))) {
+        output.appendChild(document.createTextNode("."));
+    }
+};
+
+function clear() {
+    output = document.querySelector(".text-content");
+    createText("|");
+    output.replaceWith(text);
+    runningArray = [];
+    operator = undefined;
+};
+
+function undo() {
+    output = document.querySelector(".text-content");
+    if ((output.textContent.slice(0, -1)) === "") {
+        createText("|");
+    }
+    else {
+        createText(output.textContent.slice(0, -1));
+    }
+    output.replaceWith(text);
+};
+
+function display(event) {
+    let value = event.target.textContent;
+    output = document.querySelector(".text-content");
+    if(Number(isNaN(output.textContent)) || (output.getAttribute("name") === "noAppend")) {
         createText(value);
         output.replaceWith(text);
     }
@@ -19,79 +57,27 @@ function display(value) {
     }
 };
 
+//currently operators variables cause abrupt operation on previous array values with repeated getInput() calls
+
 function getInput(event) {
+    runningArray.push(Number((document.querySelector(".text-content")).textContent))
     let value = event.target.textContent;
-    if (isNaN(value)) {
-        output = document.querySelector(".text-content");
-        arrayItem = Number(output.textContent);
-        runningArray.push(arrayItem);
-        if (runningArray.length === 1) {
-            createText("|");
-            output.replaceWith(text);
-            operator = value;
-            return operator;
-        }
-        else {
-            let result = operate(operator, runningArray[runningArray.length-2], runningArray[runningArray.length-1]);
-            createText(result);
-            output.replaceWith(text);
-            if (!(isNaN(result))) {
-                runningArray.push(result);
-            }
-        }
-    }
-    else {
-        display(value);
-    }
-};
-
-function clear() {
     output = document.querySelector(".text-content");
-    text = document.createElement("p");
-    text.setAttribute("class", "text-content");
-    text.textContent = "|";
-    output.replaceWith(text);
-    runningArray = [];
-    operator = undefined;
-};
-
-
-//key = document.querySelector(`button[data-key="${event.keyCode}"]`); <- can you do element.querySelector?
-
-
-function add (a, b) {
-    return a + b;
-};
-
-function subtract (a, b) {
-    return a - b;
-};
-
-function multiply (a, b) {
-    return a * b;
-};
-
-function divide (a, b) {
-    if (b === 0) {
-        return "A hollow voice says 'fool'";
+    if (runningArray.length === 1 || !operator) {
+        createText("|");
+        output.replaceWith(text);
+        operator = value;
+        return operator;
     }
-    return a / b;
+    else { 
+        let result = operate(operator, runningArray[runningArray.length-2], runningArray[runningArray.length-1]);
+        createText(result);
+        text.setAttribute("name", "noAppend");
+        output.replaceWith(text);
+        operator = value;
+        return operator;
+    }
 };
-
-function doMath () {
-    //if (runningArray.length === 1) {
-    //    output = document.querySelector(".text-content");
-    //    arrayItem = Number(output.textContent);
-    //    text = document.createElement("p");
-    //    text.setAttribute("class", "text-content");
-    //    runningArray.push(arrayItem);
-    //}
-    //operate(operator, runningArray[runningArray.length-2], runningArray[runningArray.length-1]);
-    //if (!(isNaN(answer))) {
-    //    runningArray.push(answer);
-    //}
-
-}
 
 function operate (operator, num1, num2) {
     let answer;
@@ -114,8 +100,38 @@ function operate (operator, num1, num2) {
     return answer;
 };
 
+function add (a, b) {
+    return a + b;
+};
+
+function subtract (a, b) {
+    return a - b;
+};
+
+function multiply (a, b) {
+    return a * b;
+};
+
+function divide (a, b) {
+    if (b === 0) {
+        return "A hollow voice says 'fool'";
+    }
+    return a / b;
+};
+
+//remember to implement rounding
+
+function createText(content) {
+    text = document.createElement("p");
+    text.setAttribute("class", "text-content");
+    text.textContent = content;
+};
+
+//key = document.querySelector(`button[data-key="${event.keyCode}"]`); <- can you do element.querySelector?
+
+
 const numBtns = Array.from(document.querySelectorAll(".number"));
-numBtns.forEach(numBtn => numBtn.addEventListener("click", getInput));
+numBtns.forEach(numBtn => numBtn.addEventListener("click", display));
 
 const opBtns = Array.from(document.querySelectorAll(".operator"));
 opBtns.forEach(opBtn => opBtn.addEventListener("click", getInput));
@@ -126,28 +142,10 @@ clearBtn.addEventListener("click", clear);
 const eqlBtn = document.querySelector("#equals");
 eqlBtn.addEventListener("click", doMath);
 
-//back button should remove the last [-1] character from .text-content with every click - upon removing the last one, should replace with |
+const undoBtn = document.querySelector("#undo");
+undoBtn.addEventListener("click", undo);
+
+const decBtn = document.querySelector("#decimal");
+decBtn.addEventListener("click", addPoint);
+
 //window.addEventListener("keydown", detectButton);
-
-
-
-
-//on first input: if it's clear, enter, or back, nothing happens
-        //if it's a number, it displays the number, replacing the dummy value
-        //if it's a decimal, append the decimal after the existing zero
-        //if it's an operand, treat 0 as the starting value for the operation
-    //on second input, if first was a number:
-        //clear and back both clear input, enter causes no change
-        //if it's a number, append it after the number
-        //if it's a decimal, append the decimal after the number and turn off button
-        //if it's an operand, store it along with the number
-    //on second input, if first was a decimal:
-        //clear and back both clear input, enter causes no change
-        //if it's a number, append it after the decimal
-        //if it's an operand, cause no change (don't store!)
-    //on second input, if first was an operand:
-        //clear and back both clear input, enter causes no change
-        //if it's a number, display the number, replacing the dummy value
-        //if it's a decimal, append the decimal after a 0
-    //continue to append numbers to the end of existing numbers
-    //num1+num2 will display num2 until the user inputs a new operand or =
